ce7097d611a354e77547f534f24f976938be23d6
G Sudarshan <11sudarshang@gmail.com>
2025-05-06 01:24:29 +0530

    EforceHTTP function

--- api/routes/shorten.go
+++ api/routes/shorten.go
@@ -17,3 +17,33 @@ type response struct {
 	XRateRemaining  int           `json:"rate_limit"`
 	XRateLimitReset time.Time     `json:"rate_limit_reset"`
 }
+
+func ShortenURL(c *fiber.Ctx) error {
+
+	body := new(request)
+	if err := c.BodyParser(&body); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "cannot parse JSON"
+		})
+	}
+
+	// implement rate limiting
+
+
+	// check if the input if an actual URL
+	if !goValidator.IsURL(body.URL){
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "invalid URL",
+		})
+	}
+
+	// check for domain error
+	if !helpers.RemoveDomainError(body.URL){
+		return c.Status(fiber.StatusServiceUnavailable).JSON(fiber.Map{
+			"error": "domain error",
+		})
+	}
+
+	// enforce https, SSL
+	body.URL = helpers.EnforceHTTP(body.URL)
+}
