275a013d188cc10b6d89b1314ed0bb20ccb1a816
G Sudarshan <11sudarshang@gmail.com>
2025-05-06 01:17:58 +0530

    ShortenURL function

--- api/main.go
+++ api/main.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"log"
+	"os"
+
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/joho/godotenv"
 )
 
 func setUpRoutes(app *fiber.App) {
@@ -11,6 +16,13 @@ func setUpRoutes(app *fiber.App) {
 
 func main() {
 	err := godotenv.Load(".env")
+	if err != nil {
+		fmt.println("Error loading .env file", err)
+	}
 	app := fiber.New()
 
+	app.Use(logger.New())
+	setUpRoutes(app)
+
+	log.Fatal(app.Listen(os.GetEnv("APP_PORT")))
 }
